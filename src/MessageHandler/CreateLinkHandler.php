c62cfc8b1ce994841c8d58b079563ab8c09bcbed
John Noel <chaostangent@gmail.com>
2022-07-17 11:39:15 +0100

    feat: Fetch link metadata when creating (not importing) links

--- src/MessageHandler/CreateLinkHandler.php
+++ src/MessageHandler/CreateLinkHandler.php
@@ -6,18 +6,24 @@ namespace App\MessageHandler;
 
 use App\Entity\Link;
 use App\Message\CreateLink;
+use App\Message\FetchLinkMetadata;
 use App\Repository\CategoryRepository;
 use App\Repository\LinkRepository;
 use App\Repository\TagRepository;
 use Symfony\Component\Messenger\Attribute\AsMessageHandler;
+use Symfony\Component\Messenger\HandleTrait;
+use Symfony\Component\Messenger\MessageBusInterface;
 
 #[AsMessageHandler]
 class CreateLinkHandler
 {
+    use HandleTrait;
+
     public function __construct(
         private readonly LinkRepository $linkRepository,
         private readonly CategoryRepository $categoryRepository,
         private readonly TagRepository $tagRepository,
+        MessageBusInterface $messageBus
     ) {
     }
 
@@ -29,6 +35,8 @@ class CreateLinkHandler
 
         $link = new Link($message->getUrl(), $categories, $tags);
         $this->linkRepository->create($link);
+
+        $this->handle(new FetchLinkMetadata($link));
 
         return $link;
     }
